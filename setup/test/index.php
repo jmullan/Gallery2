275d6be3c4457abefa6150bdcb6d1aa028bca9cd
Bharat Mediratta <bharat@menalto.com>
2002-05-09 06:48:25 +0000

    Imported from the Gallery1 code base.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@1750 57fcd75e-5312-0410-8df3-f5eb6fbb1595

---
+++ setup/test/index.php
@@ -0,0 +1,100 @@
+<?php
+require('init.php');
+require('TestCase.class');
+
+$testDir = 'TestCase';
+if ($dir = opendir($testDir)) {
+    while (($file = readdir($dir)) != false) {
+	if (preg_match('/.class$/', $file)) {
+	    require_once($testDir . '/' . $file);
+
+	    $name = str_replace('.class', '', $file);
+	    $className = $name . 'TestCase';
+	    $class = new $className();
+	    $testTable[$name] = $class;
+	}
+    }
+    closedir($dir);
+} else {
+    print "Unable to open test dir";
+}
+
+print '<center>';
+print '<h1> Test Harness </h1>';
+print '<table border=1 cellspacing=0 cellpadding=5>';
+print '<tr>';
+print '<th bgcolor=#9999CC> Name';
+print '<th bgcolor=#9999CC> Description';
+print '</tr>';
+
+asort($testTable);
+$evenOdd = 0;
+foreach ($testTable as $name => $class) {
+    print '<tr>';
+    if ($evenOdd++ % 2) {
+	$bgColor = '#EEEEEE';
+    } else {
+	$bgColor = '#CCCCCC';
+    }
+    print '<td bgColor=' . $bgColor . '>';
+    print '<a href=index.php?runTest=' . $name . '>';
+    print $name;
+    print '</a>';
+    print '</td>';
+    print '<td bgColor=' . $bgColor . '>';
+    print $class->getDescription();
+    print '</td>';
+    print '</tr>';
+}
+
+print '</table>';
+print '</center>';
+
+print '<br>';
+print '<hr>';
+
+if (!empty($HTTP_GET_VARS['runTest'])) {
+    $runTest = $HTTP_GET_VARS['runTest'];
+
+    $class = $testTable[$runTest];
+
+    if ($class->useDefaultDatabase()) {
+	include('connectToDbDefault.php');
+    } else {
+	include('connectToDbNoDefault.php');
+    }
+
+
+    print '<b>Test: ' . $runTest . '</b>';
+    print '<br>';
+    
+    print '<b>Start</b><br>';
+    set_time_limit(3);
+    $ret = $class->start();
+    if ($ret == SUCCESS) {
+	print 'Status: Success<br>';
+    } else {
+	$codes = join(", ", $gallery->getErrorAsStrings($ret));
+	print "Status: Error ($codes)<br>";
+    }
+
+    print '<br>';
+    
+    print '<b>Cleanup</b><br>';
+    $ret = $class->cleanup();
+    if ($ret == SUCCESS) {
+	print 'Status: Success<br>';
+    } else {
+	$codes = join(", ", $gallery->getErrorAsStrings($ret));
+	print "Status: Error ($codes)<br>";
+    }
+
+    print '<hr>';
+    print '<b>Debug Output</b>';
+    print '<pre>';
+    foreach ($class->getDebugOutput() as $line) {
+	print $line;
+    }
+    print '</pre>';
+}
+?>
