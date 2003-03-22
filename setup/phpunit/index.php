36354f1cfa1b4c258a1c3d769359abb03679dd6a
Bharat Mediratta <bharat@menalto.com>
2003-03-22 07:59:40 +0000

    Fixed to work when cookies are disabled.  Beautified the output.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@2561 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- setup/phpunit/index.php
+++ setup/phpunit/index.php
@@ -88,6 +88,19 @@ if ($ret->isError()) {
     return;
 }
 
+$session = $gallery->getSession();
+if (!$session->isUsingCookies()) {
+    list ($sessionKey, $sessionId) = $session->getSessionDescriptor();
+    $sessionKey = GALLERY_FORM_VARIABLE_PREFIX . $sessionKey;
+}
+
+list ($ret, $isSiteAdmin) = 
+  GalleryUserGroupMap::isUserInSiteAdminGroup($gallery->getActiveUserId());
+if ($ret->isError()) {
+    print $ret->getAsHtml();
+    return;
+}
+
 /* Uncomment this to see debug output before tests run
 print "<pre>";
 print $gallery->getDebugBuffer();
@@ -103,11 +116,54 @@ print "</pre>";
     </STYLE>
   </head>
   <body>
+    <center>
     <h1>Gallery Unit Tests</h1>
+    </center>
+    <br>
+    This is the Gallery test framework.  We'll use this to verify that
+    the Gallery code is functioning properly.  It'll help us identify
+    bugs in the code when we add new features, port to new systems, or
+    add support for new database back ends.  Some of these tests will
+    not pass unless you are logged in a Gallery site administrator.
+    When logged on, all the tests below should pass (each line will
+    have a green box that says <b>OK</b> in it).
+
+    <?php if (!$isSiteAdmin) { ?>
+    <br><br><br>
+    <font color="red">
+    <b>
+    <h2>ERROR!</h2>
+    You are not logged in as a Gallery site administrator so some
+    some of the tests <i>will</i> fail.  In order to properly use
+    these tests, you should log in as a site administrator.  If you
+    have cookies disabled, then you must go back to the page where you
+    logged in and copy the part of your URL that looks like this:
+    <i>g2_GALLERYSID=51c0ca5a9ce1296ccfd5307fa77fd998</i>, get rid of
+    the <i>g2_GALLERYSID</i> part and paste it into this text box then
+    click the Reload Page button.  That will transfer your session from
+    the page where you logged in over to this page.
     <form>
+      <input type="text" size=33 name="<?php echo $sessionKey?>">
+      <input type="submit" value="Reload page">
+    </form>
+    </b>
+    </font>
+    <?php } ?>
+    
+    <br>
+    <br>
+    
+    <form>
+    <?php if (isset($sessionKey)) { ?>
+    <input type="hidden" name="<?php echo $sessionKey?>" value="<?php echo $sessionId ?>">
+    <? } ?>
+    <h2>Filtering</h2>
+    Enter a filter string in the box below to restrict testing to classes containing 
+    that text in their name.
+    <br>
     Filter: <input type="text" name="filter" value="<?php echo $filter?>">
     </form>
-    <h2>Test Results</h2>
+    
     <?php
     $result = new PrettyTestResult();
     $testSuite->run($result);
