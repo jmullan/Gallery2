9a6f94ddb1f37e01085678faf72da7c6d70339eb
Bharat Mediratta <bharat@menalto.com>
2003-04-12 21:34:16 +0000

    Replaced Japanese translation with the UTF-8 version from Kennichi Uehara and updated all .po files.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@2608 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- lib/tools/po/update-all-translations.pl
+++ lib/tools/po/update-all-translations.pl
@@ -6,6 +6,7 @@ use File::Find;
 use Cwd;
 
 my %PO_DIRS = ();
+my %MO_FILES = ();
 
 my $basedir = cwd();
 $basedir =~ s{/(lib|docs|layouts|modules|setup|templates|themes)/.*}{};
@@ -17,11 +18,27 @@ foreach my $poDir (keys(%PO_DIRS)) {
   system("gmake install clean");
 }
 
+# Make all .mo files binary in CVS.  We could make this smarter by reading
+# the CVS/Entries files
+#
+find(\&locateMoFile, $basedir);
+chdir $basedir;
+system("cvs admin -kb " . join(" ", keys(%MO_FILES)));
+
 sub locatePoDir {
   my $file = $File::Find::name;
   my $dir  = $File::Find::dir;
   if (basename($dir) eq 'po') {
     next if ($dir =~ m|lib/tools|);
     $PO_DIRS{$dir}++;
+  }
+}
+
+sub locateMoFile {
+  my $file = $File::Find::name;
+  my $dir  = $File::Find::dir;
+  if ($file =~ /\.mo$/) {
+    $file =~ s|$basedir/||;
+    $MO_FILES{$file}++;
   }
 }
