4226ac4d4dd577861bc20d1ee9d7acdb8dcb2b2c
Bharat Mediratta <bharat@menalto.com>
2003-06-05 05:27:53 +0000

    complete rewrite.  now it only works from files in CVS
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@2902 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- lib/tools/makeDebugApprovedList.pl
+++ lib/tools/makeDebugApprovedList.pl
@@ -1,37 +1,87 @@
 #!/usr/local/bin/perl
 #
+# This script traverses the Gallery tree and creates a new directory
+# called "debug" containing a single file called viewableFiles.txt inside
+# every module and layout.  This new file contains a list of every
+# PHP file in the module/layout and a checksum for the file.
+#
+# The Debug module uses this file to figure out whether or not it's
+# safe to show you the contents of the file in stack traces.
+#
 use strict;
 use File::Find;
 use Cwd;
+use Data::Dumper;
 
 my @FILES;
 my $basedir = cwd();
 $basedir =~ s{/(lib|docs|layouts|modules|setup|templates|themes)/.*}{};
 chdir($basedir);
 
-foreach my $module (<$basedir/modules/*>) {
-  next if ($module =~ /CVS$/);
-  @FILES = ();
+# Get a list of every non-binary file committed to CVS.
+#
+my @files = ();
+&parseCvs($basedir, \@files);
 
-  system("mkdir -p $module/debug");
-  find(\&viewableFiles, $module);
+# Strip base dir
+map(s{$basedir/}{}, @files);
 
-  if ($module =~ m|/core|) {
-    push(@FILES, "init.php");
-    push(@FILES, "main.php");
-  }
+# Keep everything at the basedir, modules, layouts.  Ditch the rest
+@files = grep(m{^(modules/.*|layouts/.*|[^/]*)$}, @files);
 
-  foreach my $file (@FILES) {
-    system("cksum -o3 @FILES > $module/debug/viewableFiles.txt");
+# Keep .class, .inc, .php files
+@files = grep(m{\.(class|inc|php)$}, @files);
+
+# Separate it out into modules/layouts
+#
+my %sections;
+foreach my $file (@files) {
+  if ($file =~ m{^modules/(.*?)/}) {
+    push (@{$sections{'modules'}{$1}}, $file);
+  } elsif ($file =~ m{^layouts/(.*?)/}) {
+    push (@{$sections{'layouts'}{$1}}, $file);
+  } else {
+    push (@{$sections{'modules'}{'core'}}, $file);
   }
 }
 
-sub viewableFiles {
-  my $file = $File::Find::name;
-  my $dir  = $File::Find::dir;
-  $file =~ s|$basedir/||;
-  if ($file =~ /(inc|class|php)$/) {
-    push(@FILES, $file);
+# Now generate the checksum file for each section
+#
+foreach my $section (keys %sections) {
+  foreach my $component (keys %{$sections{$section}}) {
+    print STDERR "$section/$component...";
+    my @files = sort @{$sections{$section}{$component}};
+    mkdir("$section/$component/debug");
+    system("cksum -o3 @files > $section/$component/debug/viewableFiles.txt");
+    print STDERR "\n";
   }
 }
 
+sub parseCvs {
+  my $activeDir = shift;
+  my $files = shift;
+
+  my $cvsDir = "$activeDir/CVS";
+  die "missing $cvsDir" unless (-d $cvsDir);
+
+  local *FD;
+  open(FD, "<$cvsDir/Entries") || die;
+  while (<FD>) {
+    next if /^D$/;
+
+    if (m|^D/([^/]*)/|) {
+      my $target = "$activeDir/$1";
+      die "$target not a dir" if (! -d $target);
+      &parseCvs($target, $files);
+    } elsif (m|/([^/]*)/|) {
+      next if (m|/-kb/$|);
+      next if (/viewableFiles.txt/);
+      my $target = "$activeDir/$1";
+      die "$target not a file" if (! -f $target);
+      die if (! -f $target);
+      push(@$files, $target);
+    } else {
+      die "Can't parse: $_";
+    }
+  }
+}
