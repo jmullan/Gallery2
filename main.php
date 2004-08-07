69375ef459aa39a578e0738b86519cc40851ad9e
Bharat Mediratta <bharat@menalto.com>
2004-08-07 07:09:24 +0000

    remove debugging code
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@7049 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- main.php
+++ main.php
@@ -19,8 +19,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-require_once('save/elapsed.inc');
-
 if (!file_exists(dirname(__FILE__) . '/config.php') ||
         filesize(dirname(__FILE__) . '/config.php') < 10) {
     header("Location: install/");
