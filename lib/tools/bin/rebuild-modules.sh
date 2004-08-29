c19768c98243233366fc33c291a5b4431c337397
Bharat Mediratta <bharat@menalto.com>
2004-08-29 22:40:48 +0000

    Updated to run cleanly on Debian
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@7364 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- lib/tools/bin/rebuild-modules.sh
+++ lib/tools/bin/rebuild-modules.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 CURDIR=`pwd`
 
+MAKE=`(which gmake || which make) 2>/dev/null`
+
 cd ../../../modules
 cvs update -kk `find . -name '*.inc' | egrep 'classes/interfaces'`
 cvs update -kk `find . -name '*.sql'`
 
 makefiles=`find . -name GNUmakefile | egrep 'classes/GNUmakefile'`
 for makefile in $makefiles; do
-    (cd `dirname $makefile` && gmake scrub && gmake && gmake clean)
+    (cd `dirname $makefile` && $MAKE scrub && $MAKE && $MAKE clean)
 done
 
 cd $CURDIR
