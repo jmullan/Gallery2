7751ac4856a1bfadf9caa05ec3dd8f4dacc9ab8e
Bharat Mediratta <bharat@menalto.com>
2004-09-18 19:47:49 +0000

    Updated integrity check to work both with and without windows carriage-returns, which will happen when users are using Gallery on CVS.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@7616 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- lib/tools/bin/makeManifest.pl
+++ lib/tools/bin/makeManifest.pl
@@ -9,56 +9,90 @@
 use strict;
 use File::Basename;
 use Cwd;
+use String::CRC32;
+
+$| = 1;
 
 chdir(dirname($0) . '/../../..');
 my $basedir = cwd();
 
 # Get a list of every file committed to CVS.
 #
-my @files = ();
-&parseCvs($basedir, \@files);
+my @entries = ();
+print STDERR "Finding all files...";
+&parseCvs($basedir, \@entries);
+print STDERR "\n";
 
 # Strip base dir, sort
-map(s{$basedir/}{}, @files);
-@files = sort @files;
+print STDERR "Sorting...";
+map(s{$basedir/}{}, @entries);
+@entries = sort @entries;
+print STDERR "\n";
 
 # Get list of .class, .inc, .php files in basedir/modules/layouts..
-my @viewable = grep(m{^(install/.*|modules/.*|layouts/.*|[^/]*)\.(class|inc|php)$}, @files);
+my @viewable = grep(m{^(install/.*|modules/.*|layouts/.*|[^/]*)\.(class|inc|php)$}, @entries);
 
 # Split into sections
 #
 my %sections;
-foreach my $file (@files) {
+print STDERR "Separating into sections...";
+foreach my $file (@entries) {
   if ($file =~ m{^((modules|layouts|themes)/.*?)/}) {
     push(@{$sections{"$1/MANIFEST"}}, $file);
   } else {
     push(@{$sections{'MANIFEST'}}, $file);
   }
 }
+print STDERR "\n";
 
 # Now generate the checksum files
 #
+print STDERR "Generating checksums...";
 foreach my $manifest (keys %sections) {
   my $out;
   open($out, ">$manifest") or die;
-  my @files = @{$sections{$manifest}};
-  while (@files) {
-    my @set = splice(@files, 0, 10);
-    local *FD;
-    open(FD, "cksum -o3 @set |") or die;
-    while (<FD>) {
-      my ($sum, $size, $file) = /^(\d+)\s+(\d+)\s+(.*)$/;
-      my $view = grep(m{^\Q$file\E$}, @viewable) ? 1 : 0;
-      print $out "$file $sum $size $view\n";
+  print $out "# File crc32 crc32(crlf) size size(crlf) viewable\n";
+  my @entries = @{$sections{$manifest}};
+  foreach my $entry (@entries) {
+    my ($file, $isBinary) = split(/ /, $entry);
+    next if ($file =~ /MANIFEST$/);
+
+    open(my $fd, "<$file");
+    my $data = join('', <$fd>);
+    my $data_crlf;
+    close($fd);
+
+    my $size;
+    my $size_crlf;
+    if ($isBinary) {
+      $data_crlf = $data;
+      $size = (stat($file))[7];
+      $size_crlf = (stat($file))[7];
+    } else {
+      if ($data =~ /\r/) {
+	$data_crlf = $data;
+	$data =~ s/\r//g;
+      } else {
+	($data_crlf = $data) =~ s/\n/\r\n/g;
+      }
+      $size = length($data);
+      $size_crlf = length($data_crlf);
     }
-    close FD;
+
+    my $cksum = crc32($data);
+    my $cksum_crlf = crc32($data_crlf);
+    my $view = grep(m{^\Q$file\E$}, @viewable) ? 1 : 0;
+    print $out "$file $cksum $cksum_crlf $size $size_crlf $view\n";
   }
   close $out;
+  print STDERR ".";
 }
+print STDERR "\n";
+printf(STDERR "Completed in %d seconds\n", time - $^T);
 
 sub parseCvs {
   my $activeDir = shift;
-  my $files = shift;
+  my $entries = shift;
 
   my $cvsDir = "$activeDir/CVS";
   die "missing $cvsDir" unless (-d $cvsDir);
@@ -70,13 +104,19 @@ sub parseCvs {
 
     if (m|^D/([^/]*)/|) {
       my $target = "$activeDir/$1";
-      die "$target not a dir" if (! -d $target);
-      &parseCvs($target, $files);
+      if (-d $target) {
+	&parseCvs($target, $entries);
+      } if (-f $target) {
+	die "$target not a dir" if (! -d $target);
+      } else {
+	# CVS doesn't always get rid of D/ entries when the dir is 
+	# pruned.  Ignore.
+      }
     } elsif (m|/([^/]*)/|) {
       next if m{/MANIFEST$};
       my $target = "$activeDir/$1";
       die "$target not a file" if (! -f $target);
-      push(@$files, $target);
+      push(@$entries, sprintf("%s %d", $target, m/-kb/));
     } else {
       die "Can't parse: $_";
     }
