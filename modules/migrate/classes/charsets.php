9e058bb5432842bc3a440ae718dc1acd559532b4
Bharat Mediratta <bharat@menalto.com>
2004-12-06 06:33:14 +0000

    phpdoc fixes.  There are plenty of errors still left to be fixed at http://gallery.menalto.com/modules/GalleryAPI/apidoc/errors.html
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@8411 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- modules/migrate/classes/charsets.php
+++ modules/migrate/classes/charsets.php
@@ -19,6 +19,11 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+/**
+ * @version $Revision$ $Date$
+ * @package Migrate
+ * @author Jesse Mullan <jmullan@visi.com>
+ */
 $charset['ISO-8859-1'] = array(128 => '',
 			       129 => '',
 			       130 => '',
