038123b29289608222a3977f0f3d9e8b8f728b44
Bharat Mediratta <bharat@menalto.com>
2005-10-05 05:55:00 +0000

    Use _ENV[TMP] instead of /tmp for portability
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@11743 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- lib/tools/bin/generate-dbxml.php
+++ lib/tools/bin/generate-dbxml.php
@@ -27,7 +27,7 @@ if (!empty($_SERVER['SERVER_NAME'])) {
 require_once(dirname(__FILE__) . '/XmlParser.inc');
 require_once('../../../../../../../lib/smarty/Smarty.class.php');
 
-$tmpdir = "/tmp/g2_" . rand(1, 30000);
+$tmpdir = "$_ENV[TMP]/g2_" . rand(1, 30000);
 if (file_exists($tmpdir)) {
     print "Tmp dir already exists: $tmpdir\n";
     exit(1);
