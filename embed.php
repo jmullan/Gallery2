c1196c9693b68ac336f67e8f6fe61c094b9629d8
Andy Staudacher <andy.st@gmail.com>
2006-02-10 15:49:48 +0000

    Allow embedding applications to get the absolute filesystem path of embed.php by fetching it via fsockopen from the same server and getting the path as a HTTP header.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@12950 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- embed.php
+++ embed.php
@@ -33,4 +33,30 @@ require_once(dirname(__FILE__) . '/modules/core/classes/GalleryDataCache.class')
 GalleryDataCache::put('G2_EMBED', 1, true);
 require(dirname(__FILE__) . '/main.php');
 require(dirname(__FILE__) . '/modules/core/classes/GalleryEmbed.class');
+
+/*
+ * Simplify finding the path to embed.php by sending it as a HTTP header
+ * Idea:
+ *   In your integration setup you need to find out
+ *     - the filesystem path for embed.php
+ *     - the g2Uri and the embedUri.
+ * You can get the embed.php path with your g2Uri by fetching 
+ * http://example.com/gallery2/embed.php?getEmbedPath=1 via fsockopen.
+ */
+$getEmbedPath = GalleryUtilities::getRequestVariablesNoPrefix('getEmbedPath');
+if (!empty($getEmbedPath)){
+    if (!headers_sent()) {
+	/*
+	 * Don't use GalleryUtilities::getRemoteHostAddress() 
+	 * since it checks headers that can be forged easily too
+	 */
+	$remotehost = GalleryUtilities::getServerVar('REMOTE_ADDR');
+	$remotehost = !empty($remotehost) ? gethostbyname($remotehost) : '';
+	$localhost = GalleryUtilities::getServerVar('HTTP_HOST');
+	$localhost = !empty($localhost) ? gethostbyname($localhost) : '127.0.0.1';
+	if (!empty($remotehost) && $remotehost == $localhost) {
+	    header('X-G2-EMBED-PATH: ' . __FILE__ );
+	}
+    }
+}
 ?>
