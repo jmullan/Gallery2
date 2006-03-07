d02a9d74ee9ecbd22ef179760539f560782e054b
Alan Harder <alan.harder@gmail.com>
2006-03-07 20:47:44 +0000

    When updating, run po files through new header.pl script to ensure correct header (CVS Id tag, GPL header, Project-Id-Version)
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@13208 57fcd75e-5312-0410-8df3-f5eb6fbb1595

---
+++ lib/tools/po/header.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+# Ensure correct header (CVS Id tag, GPL header, Project-Id-Version)
+# Usage: perl -pi{ext} header.pl {po_file}
+# messages.po should exist in current dir
+
+if ($. == 1) {
+    $_ = "# \$Id\$\n" unless /^# \$Id/;
+} elsif (not $x) {
+    $x = 1 if /^(?:([^#])|#.*USA)/;
+    undef $_ unless $1;
+    print `sed -n "2,/USA/ p" messages.po` if $x;
+} elsif (s/^("Project-Id-Version:).*$/$1 Gallery: /) {
+    chomp;
+    if ($f = -f '../module.inc' ? '../module.inc' : (-f '../theme.inc' ? '../theme.inc' : '')) {
+      $m = `perl -naF\\' -e 'do { print \$F[1]; exit } if /setName/' $f`;
+      $m .= ' Theme' if ($f eq '../theme.inc');
+      $v = `perl -naF\\' -e 'do { print \$F[1]; exit } if /setVersion/' $f`;
+    } else {
+      $m = (($m = `pwd`) =~ m|install.po|) ? 'Installer' : 'Upgrader';
+      $f = '../../modules/core/module.inc';
+      $v = `perl -naF\\' -e 'do { print \$F[1]; exit } if /setGalleryVersion/' $f`;
+      $v =~ s/-.*$//;
+    }
+    $_ .= "$m $v\\n\"\n";
+}
+
