2b04294f6437153b603cf5cff271e3b138ebe6d0
Alan Harder <alan.harder@gmail.com>
2006-03-29 05:51:49 +0000

    change an if to elsif and remove a line that isn't useful
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@13355 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- lib/tools/bin/makeManifest.pl
+++ lib/tools/bin/makeManifest.pl
@@ -154,14 +154,13 @@ sub parseCvs {
       my $target = "$activeDir/$1";
       if (-d $target) {
 	&parseCvs($target, $entries);
-      } if (-f $target) {
+      } elsif (-f $target) {
 	die "$target not a dir" if (! -d $target);
       } else {
 	# CVS doesn't always get rid of D/ entries when the dir is
 	# pruned.  Ignore.
       }
     } elsif (m|/([^/]*)/([^/]*)/|) {
-      next if m{/MANIFEST$};
       my $target;
       if ($2 =~ '^-') {
 	# Deleted file
