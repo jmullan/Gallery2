e2708b977b80914cda51649c03a8d2e779a513fe
Alan Harder <alan.harder@gmail.com>
2006-04-07 04:22:45 +0000

    checkPreamble in CodeAudit test never worked. fixed escaping in regexp, fixed all errors it now found (many!) and added needed exceptions.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@13414 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- lib/tools/repository/index.php
+++ lib/tools/repository/index.php
@@ -1,5 +1,7 @@
 <?php
 /*
+ * $RCSfile$
+ *
  * Gallery - a web based photo album viewer and editor
  * Copyright (C) 2000-2006 Bharat Mediratta
  *
@@ -15,9 +17,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * $Id$
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
  /**
