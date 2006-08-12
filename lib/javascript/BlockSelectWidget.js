jmullan/Gallery2

commit 7b883f1bc513de789ca204cb3dd51a8a29efe9c8

Add support for a new 'text' type parameter for blocks.
Based on some work by donkenjo[1], this allows us to store arbitrary
text values in the parameters for a block.  Arbitrary text values
break our existing neutral data format, so I switched over to using
JSON.  This required a lot of little fiddly work in the Javascript --
now we have to convert quotes to &quot; so that we can use them in
generated JS, eg:

  var x = "some data with &quot; in it";

and then in the JS code we have to convert the &quot; back so that
the JS code has the raw data.  We also have to escape our quote values
carefully.  A little tricky, but it works.

[1]  http://gallery.menalto.com/node/51793



git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@14115 57fcd75e-5312-0410-8df3-f5eb6fbb1595

lib/javascript/BlockSelectWidget.js
@@ -91,17 +91,27 @@ function bsw_BlockPreference(key, blockId, values) {
     };
 
     this.toString = function() {
-	var result = "[" + this.blockId;
+	var result = '["' + this.blockId + '",{';
+	var count = 0;
 	for (var paramName in this.parameters) {
 	    var paramValue = this.parameters[paramName];
 	    var defaultValue =
 		bsw_WIDGET_BLOCKS[key][blockId]['parameters'][paramName]['defaultValue'];
+	    var paramType = bsw_WIDGET_BLOCKS[key][blockId]['parameters'][paramName]['type'];
 	    if (paramValue != defaultValue) {
-		result += " " + paramName + "=" + paramValue;
+		if (count++) {
+		    result += ',';
+		}
+
+		result += '"' + paramName + '":';
+		if (paramType == 'text') {
+		    result += '"' + paramValue.replace(/\"/g,'\\"') + '"';
+		} else {
+		    result += paramValue;
+		}
 	    }
 	}
-
-	result += "]";
+	result += '}]';
 
 	return result;
     };
@@ -153,7 +163,15 @@ function bsw_addUserBlockPreference(key, blockId, values) {
 function bsw_initAdminForm(key, parameterText, valueText) {
     var availableEl = document.getElementById("blocksAvailableList_" + key);
     var usedEl = document.getElementById("blocksUsedList_" + key);
-    var usedBlockString = document.getElementById("albumBlockValue_" + key).value;
+
+    /*
+     * Undo the " -> &quot; coercion that we had to do in order to embed the
+     * serialized value into a string value in HTML.
+     */
+    var valueEl = document.getElementById("albumBlockValue_" + key);
+    valueEl.value = valueEl.value.replace('&quot;', '"');
+    var usedBlockString = valueEl.value;
+
     bsw_createBlockPrefsFromString(key, usedBlockString);
     bsw_resizeBlocksUsedList(key);
     bsw_selectNone(key);
@@ -171,7 +189,7 @@ function bsw_reInitAdminForm(key) {
     if (bsw_UPDATE_LEVEL == 0) {
 	var usedBlockString = document.getElementById("albumBlockValue_" + key).value;
 	bsw_USER_BLOCK_POSITIONS[key] = new Array();
-	bsw_createBlockPrefsFromString(key, usedBlockString);
+	bsw_createBlockPrefsFromString(key, unescape(usedBlockString));
 	bsw_selectNone(key);
     }
 }
@@ -487,6 +505,13 @@ function bsw_getValueElement(paramName, blockPref, block) {
 	returnElement.selectedIndex = -1;
 
 	returnElement.onchange = function() { bsw_updatePrefValue(block.key, elementType, this); };
+    } else if (elementType == 'text') {
+	returnElement = document.createElement("input");
+	returnElement.type = 'text';
+	callback = new Function('document.getElementById("' + id + '").value = "' +
+				prefValue.replace(/\"/g, '\\"') + '";');
+	returnElement.value = prefValue;
+	returnElement.onchange = function() { bsw_updatePrefValue(block.key, elementType, this); };
     }
 
     returnElement.id = id;
@@ -513,8 +538,11 @@ function bsw_updatePrefValue(key, varType, element) {
     var paramValue;
     if (varType == 'boolean') {
 	paramValue = element.checked;
-    } else {
+    } else if(varType == 'choice') {
 	paramValue = element.options[element.selectedIndex].value;
+    } else {
+	/* text */
+	paramValue = element.value;
     }
 
     blockPref.setParameterValue(paramName, paramValue);
@@ -538,9 +566,6 @@ function bsw_updateVarOverrides(key, blockPref) {
     }
 }
 
-/* Create a regexp pattern that will pull out all of the block definitions */
-var BLOCK_REGEXP = /\[[^\]]+\]/g;
-
 /**
  * Given a string value as stored in the gallery database, create block
  * preference objects with the supplied values.
@@ -555,24 +580,10 @@ function bsw_createBlockPrefsFromString(key, stringValue) {
 
     /* Now clear out the block array */
     bsw_USER_BLOCKS[key] = new Array();
+    eval('var blockData = ' + stringValue);
 
-    var result = stringValue.match(BLOCK_REGEXP);
-    if (result == null) {
-	return;
-    }
-
-    /* At this point we have a bunch of matches with the enclosing []'s */
-    for (var i = 0; i < result.length; i++) {
-	var bits = result[i].substring(1, result[i].length - 1).split(" ");
-
-        var blockId = bits[0];
-	var prefValues = new Array();
-	for (var bitIndex = 1; bitIndex < bits.length; bitIndex++) {
-	    var paramBits = bits[bitIndex].split("=");
-	    prefValues[paramBits[0]] = paramBits[1];
-	}
-
-	bsw_addUserBlockPreference(key, blockId, prefValues);
+    for (var i = 0; i < blockData.length; i++) {
+	bsw_addUserBlockPreference(key, blockData[i][0], blockData[i][1]);
     }
 }
 
@@ -582,8 +593,9 @@ function bsw_createBlockPrefsFromString(key, stringValue) {
  */
 function bsw_updateAlbumBlockValue(key) {
     var albumValueEl = document.getElementById("albumBlockValue_" + key);
-    var value = "";
+    var newValue = '[';
 
+    var count = 0;
     for (var i = 0; i < bsw_USER_BLOCK_POSITIONS[key].length; i++) {
 	var blockPrefId = bsw_USER_BLOCK_POSITIONS[key][i];
 	var blockPref = bsw_USER_BLOCKS[key][blockPrefId];
@@ -593,16 +605,19 @@ function bsw_updateAlbumBlockValue(key) {
 	 * when the old block string contained blocks that are no longer in the master list.
 	 */
 	if (bsw_WIDGET_BLOCKS[key][blockPref.blockId]) {
-	    value += blockPref.toString() + " ";
+	    if (count++) {
+		newValue += ',';
+	    }
+	    newValue += blockPref.toString();
 	}
     }
+    newValue += ']';
 
     /*
      * Don't change it unless we really need to, to avoid unnecessarily
      * propagating onchange events which can lead to recursive calls back
      * to bsw_updateAlbumBlockValue()
      */
-    var newValue = value.substring(0, value.length - 1);
     if (albumValueEl.value != newValue) {
 	albumValueEl.value = newValue;
 	if (albumValueEl.onchange) {

modules/core/classes/GalleryTheme.class
@@ -355,49 +355,44 @@ class GalleryTheme extends GalleryPlugin {
 	switch ($packType) {
 	case 'block-list':
 	    /*
-	     * Expecting format:
-	     * [module.BlockName param1=value1 ...] [module.BlockName param1=value1 ...] ...
+	     * Expecting format (without line breaks):
+	     * [["module.BlockName",{"param1":"value1",...}],
+	     *  ["module.BlockName",{"param1":"value1",...}],
+	     *  ["module.BlockName2"]]
 	     *
 	     * Output is a serialized array of the format:
 	     * array(
-	     *    array('module.BlockName', array('param1' => 'value1'))
-	     *    array('module.BlockName', array('param1' => 'value1')))
+	     *    array('module.BlockName', array('param1' => 'value1')),
+	     *    array('module.BlockName', array('param1' => 'value1')),
+	     *    array('module.BlockName2', array()))
 	     *
 	     * The value can be empty.  There can be any number of blocks.  Blocks can be repeated.
 	     * There can be any number of parameters
 	     */
 	    $results = array();
-	    if (empty($value)) {
+	    if (empty($value) || $value == '[]') {
 		/* success */
-	    } else if (preg_match_all("/\[([\w]+\.[\w]+)\s*(.*?)\]/", $value, $matches)) {
-		for ($i = 0; $i < count($matches[0]); $i++) {
-		    $record = array($matches[1][$i], array());
-		    $matches[2][$i] = trim($matches[2][$i]);
-		    if (!empty($matches[2][$i])) {
-			$params = preg_split('/\s+/', $matches[2][$i]);
-			foreach ($params as $param) {
-			    $paramKeyValues = explode('=', $param);
-			    if (count($paramKeyValues) == 2) {
-				if (!strcasecmp($paramKeyValues[1], "false")) {
-				    $paramKeyValues[1] = false;
-				} else if (!strcasecmp($paramKeyValues[1], "true")) {
-				    $paramKeyValues[1] = true;
-				}
-				$record[1][$paramKeyValues[0]] = $paramKeyValues[1];
-			    } else {
-				$success = false;
-			    }
+	    } else {
+		GalleryCoreApi::requireOnce('lib/JSON/JSON.php');
+		$json = new Services_JSON(SERVICES_JSON_LOOSE_TYPE);
+
+		/* The sanitizer will escape quotes, so unescape them */
+		$value = str_replace('&quot;', '"', $value);
+
+		$results = $json->decode($value);
+
+		if (empty($results) || !isset($results[0])) {
+		    $success = false;
+		    $results = array();
+		} else {
+		    for ($i = 0; $i < count($results); $i++) {
+			if (count($results[$i]) == 1) {
+			    array_push($results[$i], array());
 			}
 		    }
-		    $results[] = $record;
 		}
-	    } else {
-		$success = false;
 	    }
 
-	    if (!$success) {
-		$results = array();
-	    }
 	    $value = serialize($results);
 	}
 
@@ -419,36 +414,39 @@ class GalleryTheme extends GalleryPlugin {
 	case 'block-list':
 	    /*
 	     * Convert to this format:
-	     * [module.BlockName param1=value1 ...] [module.BlockName param1=value1 ...] ...
+	     * [["module.BlockName", {"param1":"value1",...}]
+	     *  ["module.BlockName", {"param1":"value1",...}]]
 	     *
 	     * Input is:
-	     * array('module.BlockName', array('param1' => 'value1'))
+	     * array('module.BlockName', array('param1' => 'value1'),
+	     *       'module.BlockName', array('param1' => 'value1'))
 	     */
 	    $result = '';
 	    $input = @unserialize($value);
 	    if (!is_array($input)) {
 		$success = false;
+	    } else if (empty($input)) {
+		$success = true;
+		$value = '[]';
 	    } else {
-		$i = 0;
-		foreach ($input as $record) {
-		    if (!is_array($record)) {
-			$success = false;
-			break;
+		GalleryCoreApi::requireOnce('lib/JSON/JSON.php');
+		$json = new Services_JSON();
+		for ($i = 0; $i < count($input); $i++) {
+		    if (empty($input[$i]) || !is_array($input[$i])) {
+			return array(false, '');
 		    }
-		    $result .= ($i ? ' ' : '') . '[' . $record[0];
-		    if (!empty($record[1])) {
-			foreach ($record[1] as $k => $v) {
-			    $v = ($v === false) ? 'false' : ($v === true ? 'true' : $v);
-			    $result .= ' ' . $k . '=' . $v;
-			}
+		    if (count($input[$i]) == 1) {
+			array_push($input[$i], array());
 		    }
-		    $result .= ']';
-		    $i++;
 		}
-		if (!$success) {
-		    $result = '';
-		}
-		$value = $result;
+		$value = $json->encode($input);
+		/*
+		 * JSON can't distinguish between regular arrays and assoc arrays if the array is
+		 * empty, so fix it up here.  This isn't essential, but it keeps our interchange
+		 * format cleaner (and we enforce this behaviour in our tests).
+		 */
+		$value = str_replace('[]', '{}', $value);
+		$success = true;
 	    }
 	}
 	return array($success, $value);

modules/core/templates/AdminThemes.tpl
@@ -230,7 +230,7 @@
 	      <input type="hidden"
 		     id="albumBlockValue_{$setting.key}" size="60"
 		     name="{g->formVar var="form[key][`$setting.key`]"}"
-		     value="{$form.key[$setting.key]}"/>
+		     value="{$form.key[$setting.key]|replace:'"':'&quot;'}"/>
 
 	      <script type="text/javascript">
 		// <![CDATA[

modules/core/templates/blocks/ThemeSettingsForm.tpl
@@ -105,7 +105,7 @@
 		   onchange="changeSetting('{$settingKey}'); bsw_reInitAdminForm('{$settingKey}');"
 		   id="albumBlockValue_{$setting.key}" size="60"
 		   name="{g->formVar var="form[key][$settingKey]"}"
-		   value="{$form.key.$settingKey}"/>
+		   value="{$form.key.$settingKey|replace:'"':'&quot;'}"/>
 
 	    <script type="text/javascript">
 	      // <![CDATA[
@@ -174,9 +174,20 @@
   var isSaved = new Array;
   var savedValues = new Array;
   var globalValues = new Array;
+
+  {*
+   * Convert " to &quot; so that Javascript can parse embedded quotes in the
+   * the data.  Convert \ to \\ so that when we move this value around the
+   * backslash is preserved (else \" turns into ").
+   *}
   {foreach from=$ThemeSettingsForm.globalParams key=key item=value}
-    globalValues['{$key}'] = '{$value}';
+    globalValues['{$key}'] = "{$value|replace:'"':'&quot;'|replace:'\\':'\\\\'}";
   {/foreach}
+
+  {* Undo the quotes coercion here in Javascript so that we have the correct data *}
+  globalValues['albumBlocks'] = globalValues['albumBlocks'].replace(/&quot;/g, '"');
+  globalValues['photoBlocks'] = globalValues['photoBlocks'].replace(/&quot;/g, '"');
+  globalValues['sidebarBlocks'] = globalValues['sidebarBlocks'].replace(/&quot;/g, '"');
 
   function toggleGlobal(key) {ldelim}
     var frm = document.getElementById('{$formId}');
@@ -207,6 +218,7 @@
       }
     }
     if (inputWidget.type != 'checkbox') inputWidget.onchange();
+    bsw_showBlockOptions(key);
   }
 
   function changeSetting(key) {

modules/core/test/phpunit/ThemeTest.class
@@ -96,7 +96,7 @@ class ThemeTest extends GalleryTestCase {
     function testPackSettingBlockList1() {
 	$this->assertEquals(
 	    array(true, serialize(array(array('module.BlockName', array('param1' => 'value1'))))),
-	    $this->_theme->packSetting('block-list', '[module.BlockName param1=value1]'));
+	    $this->_theme->packSetting('block-list', '[["module.BlockName",{"param1":"value1"}]]'));
     }
 
     function testPackSettingBlockList2() {
@@ -104,28 +104,37 @@ class ThemeTest extends GalleryTestCase {
 			array('param1' => 'value1')),
 		    array('module2.Block2', array()),
 		    array('module.BlockName', array('param1' => 'value1', 'param2' => 'value2'))))),
-	    $this->_theme->packSetting('block-list', '[module.BlockName param1=value1] '
-		. '[module2.Block2] '
-		. '[module.BlockName param1=value1 param2=value2]'));
+	    $this->_theme->packSetting(
+		'block-list',
+		'[["module.BlockName", {"param1":"value1"}],' .
+		' ["module2.Block2"],' .
+		' ["module.BlockName",{"param1":"value1","param2":"value2"}]]'));
     }
 
     function testPackSettingBlockListTrueFalse() {
 	$this->assertEquals(array(true, serialize(array(array('module.BlockName',
 			array('param1' => false, 'param2' => true))))),
 	    $this->_theme->packSetting('block-list',
-		'[module.BlockName param1=false param2=true]'));
+		'[["module.BlockName",{"param1":false,"param2":true}]]'));
     }
 
     function testPackSettingBlockListWhitespace1() {
 	$this->assertEquals(
 	    array(true, serialize(array(array('a.B', array('x' => 'y')), array('b.c', array())))),
-	    $this->_theme->packSetting('block-list', '[a.B x=y][b.c]'));
+	    $this->_theme->packSetting('block-list', '[["a.B",{"x":"y"}],    ["b.c"]]'));
     }
 
     function testPackSettingBlockListWhitespace2() {
 	$this->assertEquals(
-	    array(true, serialize(array(array('a.B', array('x' => 'y')), array('b.c', array())))),
-	    $this->_theme->packSetting('block-list', "[a.B\n\r\tx=y]\n\n\t\n[b.c]"));
+	    array(true, serialize(
+		      array(array('a.B', array('x' => "y \n y")), array('b.c', array())))),
+	    $this->_theme->packSetting('block-list', '[["a.B",{"x":"y \n y"}],["b.c"]]'));
+    }
+
+    function testPackSettingBlockListQuotedValue() {
+	$this->assertEquals(
+	    array(true, serialize(array(array('a.B', array('x' => 'foo "bar" baz'))))),
+	    $this->_theme->packSetting('block-list', '[["a.B",{"x":"foo \"bar\" baz"}]]'));
     }
 
     function testPackSettingBlockListEmpty() {
@@ -159,36 +168,48 @@ class ThemeTest extends GalleryTestCase {
     }
 
     function testPackSettingBlockListVeryLongList() {
-	$count = 100;
-	$value = str_repeat('[a.b c=d]', $count);
+	$count = 3;
+	$data_pattern = '["a.b",{"c":"d"}]';
+	$expected_pattern = array('a.b', array('c' => 'd'));
+
+	$data = '';
 	for ($i = 0; $i < $count; $i++) {
-	    $expected[] = array('a.b', array('c' => 'd'));
+	    $expected[] = $expected_pattern;
+	    if ($i) {
+		$data .= ',';
+	    }
+	    $data .= $data_pattern;
 	}
-	$this->assertEquals(array(true, serialize($expected)),
-			    $this->_theme->packSetting('block-list', $value));
+	$data = '[' . $data . ']';
+
+	$actual = $this->_theme->packSetting('block-list', $data);
+
+	$this->assertEquals(array(true, $expected),
+			    array($actual[0], unserialize($actual[1])));
     }
 
     function testUnpackSettingBlockList1() {
 	$this->assertEquals(
-	    array(true, '[a.B foo=bar]'),
+	    array(true, '[["a.B",{"foo":"bar"}]]'),
 	    $this->_theme->unpackSetting('block-list',
 					 serialize(array(array('a.B', array('foo' => 'bar'))))));
     }
 
     function testUnpackSettingBlockList2() {
-	$this->assertEquals(array(true, '[a.B foo=bar] [c.D]'),
+	$this->assertEquals(
+	    array(true, '[["a.B",{"foo":"bar"}],["c.D",{}]]'),
 	    $this->_theme->unpackSetting('block-list',
 		serialize(array(array('a.B', array('foo' => 'bar')), array('c.D')))));
     }
 
     function testUnpackSettingBlockListTrueFalse() {
-	$this->assertEquals(array(true, '[a.B foo=true bar=false]'),
+	$this->assertEquals(array(true, '[["a.B",{"foo":true,"bar":false}]]'),
 	    $this->_theme->unpackSetting('block-list',
 		serialize(array(array('a.B', array('foo' => true, 'bar' => false))))));
     }
 
     function testUnpackSettingBlockListEmpty() {
-	$this->assertEquals(array(true, ''),
+	$this->assertEquals(array(true, '[]'),
 			    $this->_theme->unpackSetting('block-list', serialize(array())));
     }
 

commit 4ef275fa835d082ebbc3403089da30d29d20e9cb

fix saving block params with choice values (use quotes around value in toString), broken in r14115.
git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@14290 57fcd75e-5312-0410-8df3-f5eb6fbb1595

lib/javascript/BlockSelectWidget.js
@@ -104,7 +104,7 @@ function bsw_BlockPreference(key, blockId, values) {
 		}
 
 		result += '"' + paramName + '":';
-		if (paramType == 'text') {
+		if (paramType == 'text' || paramType == 'choice') {
 		    result += '"' + paramValue.replace(/\"/g,'\\"') + '"';
 		} else {
 		    result += paramValue;
@@ -252,7 +252,7 @@ function bsw_selectToChange(key) {
     if (usedEl.selectedIndex > 0) {
 	bsw_enableButton(key, "MoveUpButton");
     } else {
-        bsw_disableButton(key, "MoveUpButton");
+	bsw_disableButton(key, "MoveUpButton");
     }
 
     if (usedEl.selectedIndex < usedEl.options.length - 1) {
@@ -423,7 +423,7 @@ function bsw_showBlockOptions(key) {
     /* Now add the parameter value rows */
     callbacks = new Array();
     for (var paramName in block.parameters) {
-        var paramRowEl = document.createElement("tr");
+	var paramRowEl = document.createElement("tr");
 
 	var paramNameEl = document.createElement("td");
 	paramNameEl.appendChild(document.createTextNode(block.parameters[paramName]['description']));
@@ -538,7 +538,7 @@ function bsw_updatePrefValue(key, varType, element) {
     var paramValue;
     if (varType == 'boolean') {
 	paramValue = element.checked;
-    } else if(varType == 'choice') {
+    } else if (varType == 'choice') {
 	paramValue = element.options[element.selectedIndex].value;
     } else {
 	/* text */