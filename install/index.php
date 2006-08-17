108562b45362f9ab68b813797db97a5262059513
Alan Harder <alan.harder@gmail.com>
2006-08-17 23:56:27 +0000

    Start of phpdoc cleanup. Also removed unused file upgrade/GalleryStub.class
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@14349 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- install/index.php
+++ install/index.php
@@ -29,6 +29,10 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
+/**
+ * Gallery Installer
+ * @package Install
+ */
 
 /* Show all errors. */
 @ini_set('display_errors', 1);
