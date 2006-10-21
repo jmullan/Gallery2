8609045347159ac88fec39aefa59a353f5f16594
Bharat Mediratta <bharat@menalto.com>
2006-10-21 21:14:32 +0000

    Prevent access to repository tools for non-site-admins
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@14925 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- lib/tools/repository/index.php
+++ lib/tools/repository/index.php
@@ -39,6 +39,12 @@ function RepositoryToolsMain() {
 
     global $gallery;
 
+
+    list ($ret, $isSiteAdmin) = GalleryCoreApi::isUserInSiteAdminGroup();
+    if ($ret) {
+	return $ret;
+    }
+
     GalleryCoreApi::requireOnce(
 	'lib/tools/repository/classes/RepositoryControllerAndView.class');
 
@@ -47,17 +53,24 @@ function RepositoryToolsMain() {
     $gallery->setConfig('repository.path', $repositoryPath);
     $gallery->setConfig('repository.templates', 'lib/tools/repository/templates/');
 
-    /* Verify our repository structure exists */
-    $platform =& $gallery->getPlatform();
-    foreach (array($repositoryPath,
-		   $repositoryPath . '/modules',
-		   $repositoryPath . '/themes') as $path) {
-	if (!$platform->file_exists($path)) {
-	    if (!$platform->mkdir($path)) {
-		return GalleryCoreApi::error(ERROR_PLATFORM_FAILURE, __FILE__, __LINE__,
-					    "Unable to create directory: $path");
+    if ($isSiteAdmin) {
+	/* Verify our repository structure exists */
+	$platform =& $gallery->getPlatform();
+	foreach (array($repositoryPath,
+		       $repositoryPath . '/modules',
+		       $repositoryPath . '/themes') as $path) {
+	    if (!$platform->file_exists($path)) {
+		if (!$platform->mkdir($path)) {
+		    return GalleryCoreApi::error(ERROR_PLATFORM_FAILURE, __FILE__, __LINE__,
+						 "Unable to create directory: $path");
+		}
 	    }
 	}
+
+	/* Load controller. */
+	$controllerName = GalleryUtilities::getRequestVariables('controller');
+	$methodName = GalleryUtilities::getRequestVariables('action');
+	$controllerPath = sprintf('%s/%s.inc', dirname(__FILE__), $controllerName);
     }
 
     /* Configure our url Generator for repository mode. */
@@ -68,13 +81,8 @@ function RepositoryToolsMain() {
     }
     $gallery->setUrlGenerator($urlGenerator);
 
-    /* Load controller. */
-    $controllerName = GalleryUtilities::getRequestVariables('controller');
-    $methodName = GalleryUtilities::getRequestVariables('action');
-    $controllerPath = sprintf('%s/%s.inc', dirname(__FILE__), $controllerName);
-
     $platform =& $gallery->getPlatform();
-    if (!$platform->file_exists($controllerPath)) {
+    if (!$isSiteAdmin || !$platform->file_exists($controllerPath)) {
 	/* Set default controller. */
 	$controllerName = 'MainPage';
 	$controllerPath = sprintf('%s/%s.inc', dirname(__FILE__), $controllerName);
@@ -105,13 +113,6 @@ if ($ret) {
     $ret = $ret;
     print $ret->getAsHtml();
     print $gallery->getDebugBuffer();
-    return;
-}
-
-list ($ret, $isSiteAdmin) = GalleryCoreApi::isUserInSiteAdminGroup();
-if ($ret) {
-    $ret = $ret;
-    print $ret->getAsHtml();
     return;
 }
 ?>
