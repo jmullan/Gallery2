e52c92a2b0ac9708d27b30416a4682fbb5c8e24b
Bharat Mediratta <bharat@menalto.com>
2007-01-14 23:07:13 +0000

    Partial fix for bug 1629483.
    I fixed one aspect of this bug by moving the repository data under
    g2data/cache and adding a function in lib/support/cache.php to clean
    it out. Long running G2 installs will probably accumulate a lot of
    packages in the cache over time so letting the user have a way to
    flush them (even if it's kind of sucky right now to have to use
    lib/support -- automated would be better) is a good idea and that's
    definitely easier if it's under g2data/cache.
    
    Bump core module 1.1.28.
    
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@15698 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- lib/support/cache.php
+++ lib/support/cache.php
@@ -6,8 +6,11 @@ function getCaches() {
 	'entity' => array(true, 'clearG2dataDir', array('cache/entity'), 'Albums and photo data'),
 	'module' => array(true, 'clearG2dataDir', array('cache/module'), 'Module settings'),
 	'theme' => array(true, 'clearG2dataDir', array('cache/theme'), 'Theme settings'),
-	'template' => array(true, 'clearG2dataDir', array('smarty/templates_c'), 'Smarty templates'),
+	'template' => array(true, 'clearG2dataDir', array('smarty/templates_c'),
+			    'Smarty templates'),
 	'tmp' => array(true, 'clearG2dataDir', array('tmp'), 'Temporary directory'),
+	'repository' => array(true, 'clearG2dataDir', array('cache/repository'),
+			      'Downloadable Plugin Cache'),
 	'derivative' => array(
 	    false, 'clearG2dataDir', array('cache/derivative'),
 	    'Thumbnails and resizes <span class="subtext">(expensive to rebuild)</span>')
