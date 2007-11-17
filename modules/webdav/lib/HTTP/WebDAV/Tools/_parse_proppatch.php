9d171357ff46a704792f9cc674a44a9a7ef67178
Bharat Mediratta <bharat@menalto.com>
2007-11-17 08:53:36 +0000

    Change the license to BSD thanks to Hartmut Holzgraefe and Helgi Porbjornsson.
    This code is derived from the following versions of the WebDAV code
    from the WebDAV CVS repository:
      Server.php r1.28
      Tools/_parse_lockinfo.php r1.2
      Tools/_parse_propfind.php r1.2
      Tools/_parse_proppatch.php r1.3
    
    Hartmut created a branch from those versions for us, available here:
    
    http://cvs.php.net/viewvc.cgi/pear/HTTP_WebDAV_Server/?pathrev=RELICENSED_FOR_GALLERY
    
    I applied the following patches:
    
    http://cvs.php.net/viewvc.cgi/pear/HTTP_WebDAV_Server/Server.php?r1=1.28&r2=1.28.4.1&pathrev=RELICENSED_FOR_GALLERY&view=patch
    http://cvs.php.net/viewvc.cgi/pear/HTTP_WebDAV_Server/Tools/_parse_propfind.php?r1=1.2&r2=1.2.4.1&pathrev=RELICENSED_FOR_GALLERY&view=patch
    http://cvs.php.net/viewvc.cgi/pear/HTTP_WebDAV_Server/Tools/_parse_proppatch.php?r1=1.3&r2=1.3.4.1&pathrev=RELICENSED_FOR_GALLERY&view=patch
    http://cvs.php.net/viewvc.cgi/pear/HTTP_WebDAV_Server/Tools/_parse_lockinfo.php?r1=1.2&r2=1.2.4.1&pathrev=RELICENSED_FOR_GALLERY&view=patch
    
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@17189 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- modules/webdav/lib/HTTP/WebDAV/Tools/_parse_proppatch.php
+++ modules/webdav/lib/HTTP/WebDAV/Tools/_parse_proppatch.php
@@ -1,36 +1,50 @@
-<?php
-//
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Hartmut Holzgraefe <hholzgra@php.net>                       |
-// |          Christian Stocker <chregu@bitflux.ch>                       |
-// +----------------------------------------------------------------------+
-//
-// $Id$
+<?php // $Id$
+/*
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2002-2007 Christian Stocker, Hartmut Holzgraefe        |
+   | All rights reserved                                                  |
+   |                                                                      |
+   | Redistribution and use in source and binary forms, with or without   |
+   | modification, are permitted provided that the following conditions   |
+   | are met:                                                             |
+   |                                                                      |
+   | 1. Redistributions of source code must retain the above copyright    |
+   |    notice, this list of conditions and the following disclaimer.     |
+   | 2. Redistributions in binary form must reproduce the above copyright |
+   |    notice, this list of conditions and the following disclaimer in   |
+   |    the documentation and/or other materials provided with the        |
+   |    distribution.                                                     |
+   | 3. The names of the authors may not be used to endorse or promote    |
+   |    products derived from this software without specific prior        |
+   |    written permission.                                               |
+   |                                                                      |
+   | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  |
+   | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    |
+   | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    |
+   | FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE       |
+   | COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,  |
+   | INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, |
+   | BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;     |
+   | LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER     |
+   | CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT   |
+   | LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN    |
+   | ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE      |
+   | POSSIBILITY OF SUCH DAMAGE.                                          |
+   +----------------------------------------------------------------------+
+*/
 
 /**
  * Helper class for parsing PROPPATCH request bodies
- * 
+ *
  * @package HTTP_WebDAV_Server
  * @author Hartmut Holzgraefe <hholzgra@php.net>
  * @version 0.99.1dev
  */
-class _parse_proppatch 
+class _parse_proppatch
 {
     /**
      * Success state flag
-     * 
+     *
      * @var boolean
      * @access public
      */
@@ -38,7 +52,7 @@ class _parse_proppatch
 
     /**
      * Found properties are collected here
-     * 
+     *
      * @var array
      * @access public
      */
@@ -46,7 +60,7 @@ class _parse_proppatch
 
     /**
      * Internal tag nesting depth counter
-     * 
+     *
      * @var int
      * @access private
      */
@@ -54,7 +68,7 @@ class _parse_proppatch
 
     /**
      *
-     * 
+     *
      * @var
      * @access
      */
@@ -62,7 +76,7 @@ class _parse_proppatch
 
     /**
      *
-     * 
+     *
      * @var
      * @access
      */
@@ -70,11 +84,11 @@ class _parse_proppatch
 
     /**
      * Constructor
-     * 
+     *
      * @param resource input stream file descriptor
      * @access public
      */
-    function _parse_proppatch($handle) 
+    function _parse_proppatch($handle)
     {
         // open input stream
         if (!$handle) {
@@ -107,8 +121,8 @@ class _parse_proppatch
                 $had_input = true;
                 $this->success &= xml_parse($parser, $line, false);
             }
-        } 
-        
+        }
+
         // finish parsing
         if ($had_input) {
             $this->success &= xml_parse($parser, '', true);
@@ -130,7 +144,7 @@ class _parse_proppatch
      * @param array tag attributes
      * @return void
      */
-    function _startElement($parser, $name, $attrs) 
+    function _startElement($parser, $name, $attrs)
     {
         // name space hanndling
         if (strstr($name, ' ')) {
@@ -142,7 +156,7 @@ class _parse_proppatch
 
         if ($this->depth == 1) {
             $this->mode = $name;
-        } 
+        }
 
         if ($this->depth == 3) {
             $prop = array('name' => $name);
@@ -175,7 +189,7 @@ class _parse_proppatch
      * @param string tag name
      * @return void
      */
-    function _endElement($parser, $name) 
+    function _endElement($parser, $name)
     {
         if (strstr($name, ' ')) {
             list($ns, $name) = explode(' ', $name);
