a6d7f05f3979586164ac23ee0736aa328470eeb3
Bharat Mediratta <bharat@menalto.com>
2007-12-22 18:55:21 +0000

    * Apply GalleryUtilities::sanitizeInputValues() to some places where we   are using alternate means if data input to avoid XSS * Apply GalleryUtilities::htmlSafe() to the |markup modifier so that raw   html results are rendered sane before display.
    Review #453
    
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery2@17248 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- lib/smarty_plugins/modifier.markup.php
+++ lib/smarty_plugins/modifier.markup.php
@@ -85,7 +85,7 @@ class GalleryNoMarkupParser {
 class GalleryHtmlMarkupParser {
     function parse($text) {
 	/* http://bugs.php.net/bug.php?id=22014 - TODO: remove empty check when min php is 4.3.2+ */
-	return empty($text) ? $text : html_entity_decode($text);
+	return empty($text) ? $text : GalleryUtilities::htmlSafe(html_entity_decode($text));
     }
 }
 
